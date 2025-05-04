4ac3574c74476fb138e3156fef766c442d4e152b
shristysingh996 <shristysingh11223@gmail.com>
2025-05-04 10:24:14 -0700

    Create two matrix using nested for loop.cpp

---
+++ two matrix using nested for loop.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int rows, cols;
+
+    cout << "Enter number of rows: ";
+    cin >> rows;
+    cout << "Enter number of columns: ";
+    cin >> cols;
+
+    int matrix1[100][100], matrix2[100][100], sum[100][100];
+
+    // Input elements for the first matrix
+    cout << "Enter elements of first matrix:\n";
+    for (int i = 0; i < rows; ++i) {
+        for (int j = 0; j < cols; ++j) {
+            cout << "Element [" << i << "][" << j << "]: ";
+            cin >> matrix1[i][j];
+        }
+    }
+
+    // Input elements for the second matrix
+    cout << "Enter elements of second matrix:\n";
+    for (int i = 0; i < rows; ++i) {
+        for (int j = 0; j < cols; ++j) {
+            cout << "Element [" << i << "][" << j << "]: ";
+            cin >> matrix2[i][j];
+        }
+    }
+
+    // Add corresponding elements of both matrices
+    for (int i = 0; i < rows; ++i) {
+        for (int j = 0; j < cols; ++j) {
+            sum[i][j] = matrix1[i][j] + matrix2[i][j];
+        }
+    }
+
+    // Display the sum matrix
+    cout << "\nSum of the matrices:\n";
+    for (int i = 0; i < rows; ++i) {
+        for (int j = 0; j < cols; ++j) {
+            cout << sum[i][j] << " ";
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
