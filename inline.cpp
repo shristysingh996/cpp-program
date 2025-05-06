a3a03618897f2a7d799f0ca29dd5ac477b269776
shristysingh996 <shristysingh11223@gmail.com>
2025-05-06 07:44:31 -0700

    Create inline.cpp

---
+++ inline.cpp
@@ -0,0 +1,19 @@
+
+#include <iostream>
+using namespace std;
+
+// Inline function to calculate the cube of a number
+inline int cube(int num) {
+    return num * num * num;
+}
+
+int main() {
+    int number;
+
+    cout << "Enter a number: ";
+    cin >> number;
+
+    cout << "The cube of " << number << " is: " << cube(number) << endl;
+
+    return 0;
+}
